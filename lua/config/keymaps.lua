4b0abc2fe4a02bdf18ff9b4d888b82483b93f0d6
Joseph Rollins <joseph@guilded.gg>
2023-07-07 10:12:08 -0700

    Add <leader>fw and disable line on non-active buffer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+  "n",
+  "<leader>fw",
+  "<cmd>lua require('telescope.builtin').find_files({ search_file = vim.fn.expand('<cword>')})<CR>",
+  { noremap = true, silent = true, desc = "Word (root dir)" }
+)
