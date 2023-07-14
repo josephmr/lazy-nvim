96b67f600786db21bebb8650aeaef8d4ec7d0643
Joseph Rollins <joseph@guilded.gg>
2023-07-14 11:38:36 -0700

    Add neorg and keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,28 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<leader>fw", function()
+  require("telescope.builtin").find_files({
+    search_file = vim.fn.expand("<cword>"),
+  })
+end, { noremap = true, silent = true, desc = "Word (root dir)" })
+
 vim.keymap.set(
   "n",
-  "<leader>fw",
-  "<cmd>lua require('telescope.builtin').find_files({ search_file = vim.fn.expand('<cword>')})<CR>",
-  { noremap = true, silent = true, desc = "Word (root dir)" }
+  "<leader>f<tab>",
+  "<cmd>Neotree focus<cr>",
+  { noremap = true, silent = true, desc = "Neotree focus" }
 )
+
+vim.keymap.set(
+  "n",
+  "<leader>'",
+  "<cmd>Telescope resume<cr>",
+  { noremap = true, silent = true, desc = "Telescope resume" }
+)
+
+vim.keymap.set("n", "gs", function()
+  require("telescope.builtin").lsp_workspace_symbols({
+    query = vim.fn.expand("<cword>"),
+  })
+end, { noremap = true, silent = true, desc = "Telescope resume" })
